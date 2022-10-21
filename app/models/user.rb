439a9168311be956174a700fe96264fed698825e
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-21 23:08:22 +0100

    add validation for name in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes, through: :posts
   has_many :comments, through: :posts
+  validates :name, presence: true
 
   def recent_posts
     posts.order(created_at: desc).limit(3)
