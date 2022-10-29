9a7d8517c848cd0bb4f155f73a1158db93014f97
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-29 09:17:02 +0100

    create post from current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,29 @@ class PostsController < ApplicationController
     # @post = @user.posts.find(params[:id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    new_post = current_user.posts.new(post_data)
+    new_post.likes_counter = 0
+    new_post.comments_counter = 0
+    respond_to do |format|
+      format.html do
+        if new_post.save
+          redirect_to "/users/#{new_post.author_id}/posts/", notice: 'Success Post Saved!'
+        else
+          render :new, status: 'Error occured with Post!'
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_data
+    params.require(:posts).permit(:title, :text)
+  end
 end
